ae23e0c6246f575d00a639a1db78f3263e475b06
mcendu <43639171+mcendu@users.noreply.github.com>
2019-03-11 18:36:57 +0800

    Add vs solution & some code snippets

---
+++ wtfmc/MojangLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+namespace wtfmc
+{
+    public class MojangLoginData : LoginData
+    {
+        string clientToken;
+        string profileID;
+        string profileName;
+    }
+    public class MojangLogin : ILoginService
+    {
+        public MojangLoginData Authenticate(string email, string passwd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckAvailable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogOut(MojangLoginData access)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MojangLoginData Refresh(MojangLoginData access)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
