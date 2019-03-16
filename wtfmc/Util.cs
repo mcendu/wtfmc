4a6a60178431c71e1ce76de4c27347a9d267ba7c
mcendu <nathandu@outlook.com>
2019-03-16 20:39:52 +0800

    Added JVM detection * Addition partially due to a joke from classmates

--- wtfmc/Util.cs
+++ wtfmc/Util.cs
@@ -4,12 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace wtfmc
 {
     public static class Util
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Util));
         /// <summary>
         /// Convert a byte array to its hex representation.
         /// </summary>
@@ -27,5 +29,92 @@ namespace wtfmc
 
         public static bool checkIntegrity(Stream hInput, string cksum)
             => bintohex(SHA1.Create().ComputeHash(hInput)) == cksum;
+
+        /// <summary>
+        /// Locate a JVM.
+        /// Would prefer Java 8 over other Java revisions.
+        /// </summary>
+        /// <remarks>
+        /// This method only searches the default installation
+        /// directory, and would not check directories outside
+        /// it.
+        /// </remarks>
+        /// <returns>The absolute path to the JVM.</returns>
+        public static string locateJava()
+        {
+            string baseSPath;
+            int selMajV = 0;
+            int selSubV = 0;
+            string selPath = null;
+            if (Directory.Exists("C:\\Program Files\\Java"))
+            {
+                baseSPath = "C:\\Program Files\\Java";
+            }
+            else if (Directory.Exists("C:\\Program Files (x86)\\Java"))
+            {
+                baseSPath = "C:\\Program Files (x86)\\Java";
+            }
+            else
+                return null;
+            IEnumerable<string> sDirs = Directory.EnumerateFiles(baseSPath);
+            foreach (string i in sDirs)
+            {
+                // Match Java 8
+                Match mSubV = Regex.Match(i, @"(?=j(dk|re)1\.8\.0_)\d");
+                if (mSubV.Success)
+                {
+                    short subV = short.Parse(mSubV.Value);
+                    if (subV > selSubV)
+                    {
+                        selSubV = subV;
+                        selPath = i;
+                    }
+                    continue;
+                }
+
+                // Match newer Java
+                mSubV = Regex.Match(i, @"(?=j(dk|re)-(?<major>\d)\.\d\.)\d");
+                if (mSubV.Success)
+                {
+                    short majV = short.Parse(mSubV.Groups["major"].Value);
+                    short subV = short.Parse(mSubV.Value);
+                    if (selMajV != 8 && majV > selMajV)
+                    {
+                        selMajV = majV;
+                        selSubV = subV;
+                        selPath = i;
+                        continue;
+                    }
+                    else if (subV > selSubV)
+                    {
+                        selSubV = subV;
+                        selPath = i;
+                    }
+                    continue;
+                }
+
+                // Match older Java
+                mSubV = Regex.Match(i, @"(?=j(dk|re)(1\.(?<major>\d)\.\d_)\d");
+                if (mSubV.Success)
+                {
+                    short majV = short.Parse(mSubV.Groups["major"].Value);
+                    short subV = short.Parse(mSubV.Value);
+                    if (majV > selMajV)
+                    {
+                        selMajV = majV;
+                        selSubV = subV;
+                        selPath = i;
+                        continue;
+                    }
+                    else if (subV > selSubV)
+                    {
+                        selSubV = subV;
+                        selPath = i;
+                    }
+                    continue;
+                }
+            }
+            return selPath;
+        }
     }
 }
--- wtfmc_unit/MojangLoginUnit.cs
+++ wtfmc_unit/MojangLoginUnit.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using wtfmc;
+using wtfmc.MojangAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace unit
 {
     [TestClass]
-    public class MojangLoginUnit
+    public class MojangLoginTest
     {
         [TestMethod]
         public void ExportDataTest()
         {
             // Taken from actual login data; accessToken has since been invalidated
             string payload = "{\"accessToken\":\"dbc5065818454f2ba7261ccf7146d64a\",\"clientToken\":\"0123456789abcdef0123456789abcdef\",\"selectedProfile\":{\"agent\":\"minecraft\",\"id\":\"f38abefd3b7d4f7a967bb1da32da2f3d\",\"name\":\"McEndu\",\"userId\":\"37147c9640e78c8f074d7695bccfcf0d\",\"createdAt\":1528198061000,\"legacyProfile\":false,\"suspended\":false,\"tokenId\":\"22049502\",\"paid\":true,\"migrated\":false},\"availableProfiles\":[{\"agent\":\"minecraft\",\"id\":\"f38abefd3b7d4f7a967bb1da32da2f3d\",\"name\":\"McEndu\",\"userId\":\"37147c9640e78c8f074d7695bccfcf0d\",\"createdAt\":1528198061000,\"legacyProfile\":false,\"suspended\":false,\"tokenId\":\"22049502\",\"paid\":true,\"migrated\":false}],\"user\":{\"id\":\"37147c9640e78c8f074d7695bccfcf0d\",\"email\":\"nathandu@outlook.com\",\"username\":\"nathandu@outlook.com\",\"registerIp\":\"64.64.108.*\",\"registeredAt\":1528033389000,\"passwordChangedAt\":1528033389000,\"dateOfBirth\":1053216000000,\"suspended\":false,\"blocked\":false,\"secured\":false,\"migrated\":false,\"emailVerified\":true,\"legacyUser\":false,\"properties\":[{\"userId\":\"37147c9640e78c8f074d7695bccfcf0d\",\"profileId\":\"\",\"name\":\"preferredLanguage\",\"value\":\"en-us\",\"languageCode\":\"en-us\"},{\"userId\":\"37147c9640e78c8f074d7695bccfcf0d\",\"profileId\":\"\",\"name\":\"registrationCountry\",\"value\":\"JP\"}],\"verifiedByParent\":false,\"hashed\":false,\"fromMigratedUser\":false}}";
-            MojangLogin aclient = new MojangLogin
+            Login aclient = new Login
             {
                 Data = payload
             };
