2c2b5559475a0a9bdb3145b1e1aeabb55fadc9de
mcendu <nathandu@outlook.com>
2019-03-17 18:49:42 +0800

    Added IVersionParser * Removed VersionLoader * Code would no longer be platform-neutral

---
+++ wtfmc/IVersionParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace wtfmc
+{
+    /// <summary>
+    /// Loads version data.
+    /// </summary>
+    public interface IVersionParser {
+
+        /// <summary>
+        /// The version data.
+        /// </summary>
+        JObject Version { get; }
+
+        /// <summary>
+        /// Check all files referenced in the
+        /// version json. The lack of both the
+        /// file itself or the integrity would
+        /// cause it to be downloaded.
+        /// </summary>
+        void checkFiles();
+
+        /// <summary>
+        /// Generate the value for the -cp option.
+        /// </summary>
+        /// <returns>A comma-separated list of jars.</returns>
+        string generateClasspath();
+    }
+}
