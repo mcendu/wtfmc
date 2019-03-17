e41634165bc2648d9b543fb35c8af0d36b3ce6f3
mcendu <nathandu@outlook.com>
2019-03-17 18:18:31 +0800

    Downloader is no longer concurrent

--- wtfmc/Downloader.cs
+++ wtfmc/Downloader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,74 +13,41 @@ namespace wtfmc
     public class Downloader
     {
         private readonly HttpClient hclient = new HttpClient();
-        private TaskFactory factory = new TaskFactory();
-        public readonly short maxThreads;
-        public Queue<Download> downQueue = null;
         private static readonly ILog log = LogManager.GetLogger(typeof(Downloader));
         
-        public Downloader(short threads)
+        public Downloader()
         {
-            maxThreads = threads;
         }
 
-        public Downloader() => new Downloader(8);
-
         /// <summary>
-        /// Download multiple files concurrently.
+        /// Download a file.
         /// </summary>
-        /// <param name="q"></param>
-        public async Task DownloadAsync() => await Task.Run(delegate ()
+        /// <param name="dl">The download data.</param>
+        public async Task DownloadAsync(Download dl)
         {
-            if (downQueue == null) downQueue = new Queue<Download>();
-            Download pend;
-            Task[] tlist = new Task[maxThreads];
-            while (downQueue.Count != 0)
+            // Initialize variables
+            log.Info($"Downloading {dl.src.AbsoluteUri}");
+            Stream from = await hclient.GetStreamAsync(dl.src);
+            FileStream to = new FileStream(dl.path, FileMode.Create, FileAccess.ReadWrite);
+            int readlen;
+            byte[] buf = new byte[2048];
+
+            // Concurrently move data from download stream to file
+            while ((readlen = from.Read(buf, 0, 2048)) != 0)
             {
-                for (int j = 0; j < maxThreads; j++)
-                {
-                    if (tlist[j] == null || tlist[j].Status != TaskStatus.Running)
-                    {
-                        pend = downQueue.Dequeue();
-                        tlist[j] = factory.StartNew(delegate (object input)
-                        {
-                            // Initialize variables
-                            Download dl = input as Download;
-                            log.Info($"Downloading {dl.from.AbsoluteUri}");
-                            Stream from = hclient.GetStreamAsync(dl.from).Result;
-                            FileStream to = new FileStream(dl.to, FileMode.Create, FileAccess.ReadWrite);
-                            int readlen;
-                            byte[] buf = new byte[2048];
-
-                            // Concurrently move data from download stream to file
-                            while ((readlen = from.Read(buf, 0, 2048)) != 0)
-                            {
-                                to.Write(buf, 0, 2048);
-                            }
-                            to.Flush();
-                            to.Position = 0;
-
-                            // Check the hash of the download
-                            if (!Util.checkIntegrity(to, dl.hash))
-                            {
-                                log.Warn($"Failed to download {dl.from.AbsoluteUri}; Requeueing");
-                                downQueue.Enqueue(dl);
-                            }
-                            else
-                            {
-                                log.Info($"Downloaded {dl.from.AbsoluteUri}");
-                            }
-                        }, pend);
-
-                        // When download queue is empty
-                        if (downQueue.Count == 0)
-                        {
-                            Task.WaitAll(tlist);
-                            break;
-                        }
-                    }
-                }
+                to.Write(buf, 0, 2048);
             }
-        });
+            to.Flush();
+            to.Position = 0;
+
+            // Check the hash of the download
+            if (!Util.checkIntegrity(to, dl.hash))
+            {
+                log.Error($"Failed to download {dl.src.AbsoluteUri}");
+                throw new Exception(); // Fault the task
+            }
+            log.Info($"Downloaded {dl.src.AbsoluteUri}");
+        }
     }
 
     /// <summary>
@@ -87,15 +55,15 @@ namespace wtfmc
     /// </summary>
     public class Download
     {
-        public readonly Uri from;
-        public readonly string to;
+        public readonly Uri src;
+        public readonly string path;
         public readonly string hash;
         public readonly long length;
 
-        public Download(Uri from, string to, string hash, long length)
+        public Download(Uri src, string path, string hash, long length)
         {
-            this.from = from;
-            this.to = to;
+            this.src = src;
+            this.path = path;
             this.hash = hash;
             this.length = length;
         }
